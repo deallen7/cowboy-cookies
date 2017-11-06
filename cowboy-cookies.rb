24302116f41dbeb1151ae07f32f667353b70c5eb
David Allen <deallen@Davids-MacBook-Pro.local>
2017-11-06 12:23:46 -0500

    Initial commit of my cowboy cookie recipe! Thanks mom :)

---
+++ cowboy-cookies.rb
@@ -0,0 +1,99 @@
+#This is a delicious recipe for cookies that allowed me and 3 friends to survive two days in the mountains 
+
+puts "Hi there! Glad you are interested in some yummy cookies."
+sleep(2)
+puts "Are you ready to bake some yum yums?"
+gets.chomp
+
+#Tool inventory
+puts "Great! Not sure why you'd be here if you weren't ready to make cookies. Let's hop to it."
+sleep(3)
+puts "First, let's make sure we have everything we need."
+sleep(2)
+puts "We'll need the following tools:"
+sleep(2)
+puts "Parchment paper"
+sleep(1.5)
+puts "Two mixing bowls"
+sleep(1.5)
+puts "Two mixing spoons"
+sleep(1.5)
+puts "A baking sheet with 1-3 inch sides"
+sleep(1.5)
+puts "And an oven! Obviously :)"
+sleep(1.5)
+puts "Ok, have everything you need?"
+gets.chomp
+
+#Ingredient Inventory
+puts "Ok, sweet. Lol, 'sweet', get it? Because we're making cookies.... LAUGH DAMMIT! Anyways..."
+sleep(4)
+puts "Let's look at all the ingredients we'll need for a batch of cookies:"
+sleep(2)
+puts "2 cups of whole wheat flour (I prefer St. Arnolds with no additives or bleach or vitamins, etc. Just pure flour.)"
+sleep(2)
+puts "1 cup of good brown sugar"
+sleep(2)
+puts "1 cup of good cane sugar (starting to sound like Ina Garten over here! XD"
+sleep(2)
+puts "3 tablespoons of blackstrap molasses"
+sleep(2)
+puts "2 good eggs with nice orange yolks."
+sleep(2)
+puts "1/2 teaspoon of alluminum-free baking powder"
+sleep(2)
+puts "1/2 teaspoon of good, clean sea salt"
+sleep(2)
+puts "1 teaspoon of baking soda"
+sleep(2)
+puts "2 sticks of butter (or 1 large stick of kerrygold grassfed unsalted butter... this is what I prefer!"
+sleep(2)
+puts "That is the cookie base. You can literally add ANYTHING YOU WANT now. Ready to move on to some of my favorite 'extras?'?"
+gets.chomp
+
+#Extras
+puts "Ok, I like to use:"
+sleep(2)
+puts "1-2 cups of oats"
+sleep(2)
+puts "1-2 bars of good dark chocolate (frozen, and then mash with a hammer to break it up). Chocolate chips work too!"
+sleep(2)
+puts "2-3 tablesppons of sesame seeds"
+sleep(2)
+puts "Cayenne pepper to taste"
+sleep(2)
+puts "1 cup of goji berries"
+sleep(2)
+puts "2-3 tablespoons of psyllium husk fiber (no flavor at all, but it contributes a chewy-firm texture and plenty of healthy fiber!)"
+sleep(2)
+puts "Add your favorite ingredients! Ideas: candy bars, almonds, cinnamon, dried cherries...You can even add maple syrup or honey instead of blackstrap molasses. These cookies are a blank canvas! Enjoy and flex your creativity!!"
+sleep(5)
+puts "Ready to get to baking?!?!?!"
+gets.chomp
+
+#Baking Process // Directions
+puts "Great! Let's start baking :)"
+sleep(1)
+puts "Pre-heat your over to 350 degrees. Let me know when to move on the the next step, please."
+gets.chomp
+puts "Combine flour, salt, baking powder and baking soda in mixing bowl. Let me know when to move on the the next step, please."
+gets.chomp
+puts "Melt butter on the stove and then combine in SEPARATE mixing bowl with sugar. Let me know when to move on the the next step, please."
+gets.chomp
+puts "Add eggs to sugar and butter once mixture is cool (you don’t want to cook those eggs). Let me know when to move on the the next step, please."
+gets.chomp
+puts "Add blackstrap molasses to butter, egg and sugar mixture. Mix well. It should look much darker. Let me know when to move on the the next step, please."
+gets.chomp
+puts "Slowly add flour mixture to sugar mixture. Add some, then mix. Add some, then mix. Let me know when to move on the the next step, please."
+gets.chomp
+puts "Once you’ve added all the flour mix, begin adding your extras. Add some, then mix. Repeat."
+gets.chomp
+puts "The mixture should be thick and goey, like cookie dough!"
+sleep(2)
+puts "At this point, you can make individual cookies or make bars."
+sleep(2)
+puts "To make cookies, just spoon globs of cookie dough onto parchment paper on a baking sheet. Leave space in between cookies. Bake at 350 for 12-15 minutes. Allow to cool. Consume! Want to learn how to make the bars?"
+gets.chomp
+puts "To make bars, layer a baking pan with high sides with parchment paper. Pour in cookie dough and smooth out. Bake at 350 for 25-35 minutes. Allow to cool. Cut in whatever lengths you like, and consume!"
+sleep(5)
+puts "That's it! Run the program again or shoot me a tweet at @deallen7 if you have any recommendations for the code!"
